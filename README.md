d674ede0cc6369b16eddf2f4fa7b60daabaad90b
Ian Atha <ian@atha.io>
2019-10-29 20:36:00 -0700

    Apply my library skeleton.

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # SyncPrimitives
 
+[![Apache License](https://img.shields.io/hexpm/l/sync_primitives)](LICENSE.md)
+[![Hex.pm](https://img.shields.io/hexpm/v/sync_primitives)](https://hex.pm/packages/sync_primitives)
+[![Documentation](https://img.shields.io/badge/hexdocs-latest-blue.svg)](https://hexdocs.pm/sync_primitives/index.html)
+[![Build Status](https://travis-ci.org/ianatha/sync_primitives.svg?branch=master)](https://travis-ci.org/ianatha/sync_primitives)
+[![Coverage Status](https://coveralls.io/repos/github/ianatha/sync_primitives/badge.svg?branch=master)](https://coveralls.io/github/ianatha/sync_primitives?branch=master)
+
 **TODO: Add description**
 
 ## Installation
@@ -9,9 +15,7 @@ by adding `sync_primitives` to your list of dependencies in `mix.exs`:
 
 ```elixir
 def deps do
-  [
-    {:sync_primitives, "~> 0.1.0"}
-  ]
+  [{:sync_primitives, "~> 0.1.0"}]
 end
 ```
 
--- mix.exs
+++ mix.exs
@@ -1,28 +1,70 @@
 defmodule SyncPrimitives.MixProject do
   use Mix.Project
 
+  @name :sync_primitives
+  @version "0.1.0"
+  @maintainers ["Ian Atha <ian@atha.io>"]
+  @github "https://github.com/ianatha/#{@name}"
+  @source_url @github
+  @homepage_url @github
+
+  @description """
+  Synchronization primitives for Elixir
+  """
+
   def project do
     [
-      app: :sync_primitives,
-      version: "0.1.0",
+      app: @name,
+      version: @version,
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      package: package(),
+      description: @description,
+      source_url: @source_url,
+      homepage_url: @homepage_url,
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.html": :test,
+        "coveralls.detail": :test,
+        "coveralls.json": :test,
+        "coveralls.post": :test,
+        "coveralls.travis": :test
+      ]
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:mix_test_watch, "~> 0.8", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
+      {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.12", only: [:test]},
+      {:dialyxir, "~> 1.0.0-rc.7", only: [:dev], runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      name: @name,
+      files: [
+        "lib",
+        "mix.exs",
+        "README.md",
+        "LICENSE.md"
+      ],
+      maintainers: @maintainers,
+      licenses: ["Apache 2.0"],
+      links: %{
+        "GitHub" => @github
+      }
     ]
   end
 end
